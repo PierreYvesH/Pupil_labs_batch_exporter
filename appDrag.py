e06fd59ce3a25d3a69e5e5bab8cf7d1fcb7c60d9
Pierre-Yves Hasan <phas@oticonmedical.com>
2020-04-17 11:38:32 +0200

    Commit app

---
+++ appDrag.py
@@ -0,0 +1,214 @@
+#builtin
+import os
+# 3rd party
+from PyQt5 import QtCore, QtGui, QtWidgets
+# local
+_dir = os.path.dirname(os.path.abspath(__file__))
+os.sys.path.append(os.path.join(_dir, 'from_pupil'))
+from from_pupil.player_methods import is_pupil_rec_dir
+from from_pupil.extract_diameter import process_recording 
+from from_pupil.extract_diameter import process_recording_annotations
+
+class dragApp(QtWidgets.QWidget):
+
+    def __init__(self):
+        if QtWidgets.QApplication.instance() is None:
+            self._app = QtWidgets.QApplication([])
+        else: 
+            self._app = QtWidgets.QApplication.instance()
+        super().__init__()
+        pix = QtGui.QPixmap(os.path.join(_dir, 'icon.png'))
+        self.icon = QtGui.QIcon(pix)
+        self.setWindowIcon(self.icon)
+        self.setWindowTitle('Batch exporter')
+
+        self.target_path = ''     
+        self.recordings = None
+        self.levels = None
+
+        self.setAcceptDrops(True)
+        self.setEnabled(True)
+        self.main_layout = QtWidgets.QHBoxLayout()
+        self.setLayout(self.main_layout)
+
+        self.setup_main_panel()
+        self.setup_side_panel()
+           
+        self.show()
+        self.resize(600, 480)
+
+    def setup_main_panel(self):
+        self.stack = QtWidgets.QStackedWidget()
+        self.stack_wait = QtWidgets.QLabel('Drop folder here!')
+        self.stack_wait.setAlignment(
+            QtCore.Qt.AlignCenter | QtCore.Qt.AlignVCenter
+            )
+
+        self.layout().addWidget(
+            self.stack, 
+            stretch = 5
+            )
+        self.stack.addWidget(self.stack_wait)
+        self.stack.setCurrentWidget(self.stack_wait)
+
+    def setup_side_panel(self):
+        # Create layout and widget
+        self.side_panel = QtWidgets.QWidget()
+        self.side_layout = QtWidgets.QVBoxLayout()
+        self.layout().addWidget(self.side_panel)
+        self.side_panel.setLayout(self.side_layout)
+
+        # Create controls
+        self.chkbox_group = QtWidgets.QCheckBox('Group exports')
+        self.chkbox_annotation = QtWidgets.QCheckBox('Export annotations')
+        self.chkbox_omit = QtWidgets.QCheckBox('Omit first level')
+        self.target = QtWidgets.QLineEdit(self.target_path)
+        self.export = QtWidgets.QPushButton('Export')
+
+        self.chkbox_group.clicked.connect(self.clicked_checkbox)
+        self.target.textChanged.connect(self.changed_text)
+        self.export.clicked.connect(self.clicked_export)
+
+        # Add controls
+        self.side_layout.addWidget(self.chkbox_group)
+        self.side_layout.addWidget(self.chkbox_annotation)
+        self.side_layout.addWidget(self.chkbox_omit)
+        self.side_layout.addWidget(self.target)
+        self.target.setVisible(False)
+        self.side_layout.addStretch(stretch = 5)
+        self.side_layout.addWidget(self.export)
+
+    # The drag and drop section
+    def dropEvent(self, e):
+        self.tree = QtWidgets.QTreeWidget()
+        self.tree.setHeaderLabel('Recordings architecture')
+        self.stack.addWidget(self.tree)
+        self.target_path = ''  
+
+        e.acceptProposedAction()
+        data =  e.mimeData()
+
+        recordings = None
+        levels = []
+        if data.hasUrls():
+            for url in data.urls():
+                # Uniform path
+                path = url.toLocalFile()
+                path = os.path.abspath(path)
+
+                (url_tree_item, url_recordings, url_levels) = getTree(path)
+                if url_tree_item is None:
+                    continue
+                
+                if recordings is None:
+                    recordings = url_recordings
+                    levels = url_levels
+                else:
+                    recordings.extend(url_recordings)
+                    levels.extend(url_levels)
+                
+                # Choose a location for grouped exports
+                if len(self.target_path) < 1:
+                    self.target.setText(os.path.join(path, 'exports'))
+
+                self.tree.addTopLevelItem(url_tree_item)
+                self.stack.setCurrentWidget(self.tree)
+                
+        if recordings is not None:
+            self.recordings = recordings
+            self.levels = levels
+
+    # Override events
+    def dragEnterEvent(self, e):
+        e.accept()
+
+    def dragMoveEvent(self, e):
+        e.accept()
+
+    def changed_text(self, s):
+        self.target_path = self.target.text()
+
+    def clicked_checkbox(self, s):
+        self.target.setVisible(not self.target.isVisible())
+
+    def clicked_export(self, s):
+        recordings = self.recordings
+        if recordings is None:
+            return
+
+        group = self.chkbox_group.isChecked()
+        omit = self.chkbox_omit.isChecked()
+        target = self.target_path
+        annotation = self.chkbox_annotation.isChecked()
+        level = self.levels
+
+        for (rec, lvl) in zip(recordings, level):
+            name = 'pupil_positions'
+            if omit:
+                lvl = lvl[lvl.index('_')+1:]
+
+            if not group:
+                target = os.path.join(rec, 'exports')
+            else:
+                name += '_' + lvl
+
+            if not os.path.exists(target):
+                os.mkdir(target)
+
+            process_recording(rec, name + '.csv', target, overwrite = True)
+            if annotation:
+                name = 'annotations'
+                if group:
+                    name += '_' + lvl
+                process_recording_annotations(
+                    rec, name + '.csv', target, overwrite = True)
+            
+def getTree(path, tree = None):
+
+    # Init
+    recordings = []
+    level = []
+    if tree is None:
+        tree = QtWidgets.QTreeWidgetItem()
+
+    # Only directories can be recordings
+    if not os.path.isdir(path):
+        return tree, recordings, level
+        
+    cur_di = path.split(os.sep)[-1]
+
+    # Is the directory a recording
+    if is_pupil_rec_dir(path):
+        recordings.append(path) 
+        tree.setText(0, cur_di) 
+        level.append(cur_di)
+
+    else: 
+        # Look into the directory
+        dirs = os.listdir(path)
+        for di in dirs:
+            # Remove the hidden files
+            if di.startswith('.'):
+                continue
+            
+            # Create a new tree for each directory
+            di_path = os.path.join(path, di)
+            di_tree = QtWidgets.QTreeWidgetItem()
+
+            # Recursion
+            (di_tree, rec, lvl) = getTree(di_path, tree = di_tree)
+            # If there was a recording we need to have the different 
+            # levels
+            if len(rec) > 0:
+                di_tree.setText(0, di)
+                tree.setText(0, cur_di)
+                tree.addChild(di_tree)
+                recordings.extend(rec)
+                new_level = [cur_di + '_' + lev for lev in lvl]  
+                level.extend(new_level)
+            
+    return tree, recordings, level    
+
+if __name__ == '__main__':
+    wid = dragApp()
+    wid._app.exec()
---
+++ from_pupil/extract_diameter.py
@@ -0,0 +1,259 @@
+import argparse
+import csv
+import logging
+import os
+import traceback as tb
+
+import numpy as np
+import msgpack
+import sys
+
+logger = logging.getLogger(__name__)
+
+def main(recordings, csv_out, out_directory = '', overwrite=False, annotations = True):
+    """Process given recordings one by one
+
+    Iterates over each recording and handles cases where no pupil.pldata or
+    pupil_timestamps.npy files could be found.
+
+    recordings: List of recording folders
+    csv_out: CSV file name under which the result will be saved
+    """
+    idx = 0
+    
+    for rec in recordings:
+        try:
+            logger.info("Extracting {}...".format(rec))
+            #pm.update_recording_to_recent(rec)
+            process_recording(rec, csv_out[idx], out_directory, overwrite=overwrite)
+            if annotations:
+                process_recording_annotations(rec, csv_out[idx+1], out_directory, overwrite=overwrite)
+
+        except FileNotFoundError:
+            logger.warning(
+                (
+                    "The recording {} did not include any prerecorded pupil files!"
+                ).format(rec)
+            )
+            logger.debug(tb.format_exc())
+        except ValueError:
+            logger.warning(rec)
+            
+        if annotations:
+            idx += 2
+        else:
+            idx += 1
+    return
+
+def process_recording(recording, csv_out, out_directory, overwrite=False):
+    """Process a single recording
+
+    recordings: List of recording folders
+    csv_out: CSV file name under which the result will be saved
+    overwrite: Boolean indicating if an existing csv file should be overwritten
+    """
+    if len(out_directory) == 0:
+        csv_out_path = os.path.join(recording, csv_out)
+    else:
+        csv_out_path = os.path.join(out_directory, csv_out)
+
+    if os.path.exists(csv_out_path):
+        if not overwrite:
+            logger.warning("{} exists already! Not overwriting.".format(csv_out_path))
+            return
+        else:
+            logger.warning("{} exists already! Overwriting.".format(csv_out_path))
+
+    with open(csv_out_path, "w", newline = '') as csv_file:
+        writer = csv.writer(csv_file, dialect=csv.get_dialect('excel'))
+        writer.writerow(csv_header())
+
+        extracted_rows = load_and_yield_data(recording)
+        writer.writerows(extracted_rows)
+    return
+
+def process_recording_annotations(recording, csv_out, out_directory, overwrite=False):
+    """Process a single recording
+
+    recordings: List of recording folders
+    csv_out: CSV file name under which the result will be saved
+    overwrite: Boolean indicating if an existing csv file should be overwritten
+    """
+    if len(out_directory) == 0:
+        csv_out_path = os.path.join(recording, csv_out)
+    else:
+        csv_out_path = os.path.join(out_directory, csv_out)
+
+    if os.path.exists(csv_out_path):
+        if not overwrite:
+            logger.warning("{} exists already! Not overwriting.".format(csv_out_path))
+            return
+        else:
+            logger.warning("{} exists already! Overwriting.".format(csv_out_path))
+
+    with open(csv_out_path, "w") as csv_file:
+        writer = csv.writer(csv_file, dialect=csv.unix_dialect, quoting = csv.QUOTE_NONE)
+        writer.writerow(csv_header_annotations())
+
+        extracted_rows = load_and_yield_annotations(recording)
+        writer.writerows(extracted_rows)
+    return
+
+def csv_header():
+    """CSV header fields"""
+    return (
+        "eye_id",
+        "timestamp",
+        "confidence",
+        "diameter",
+        "diameter_3d",
+    )
+
+def csv_header_annotations():
+    """CSV header fields"""
+    return (
+        "timestamp",
+        "label"
+    )
+
+def load_and_yield_annotations(directory, topic = 'notify'):
+    try:
+        ts_file = os.path.join(directory, topic + "_timestamps.npy")
+        data_ts = np.load(ts_file)
+
+        msgpack_file = os.path.join(directory, topic + ".pldata")
+        with open(msgpack_file, "rb") as fh:
+            unpacker = msgpack.Unpacker(fh, raw=False, use_list=False)
+            for timestamp, (topic, payload) in zip(data_ts, unpacker):
+                datum = deserialize_msgpack(payload)
+                label = extract_eyeid_messages(datum)
+                yield(timestamp, label)
+    except FileNotFoundError:
+        logger.warning("{} cannot be processed - file does not exist".format(ts_file))
+        logger.warning("Creating fake annotations")
+
+    return
+
+def load_and_yield_data(directory, topic="pupil"):
+    """Load and extract pupil diameter data
+
+    See the data format documentation[2] for details on the data structure.
+
+    Adapted open-source code from Pupil Player[1] to read pldata files.
+    Removed the usage of Serialized_Dicts since this script has the sole purpose
+    of running through the data once.
+
+    [1] https://github.com/pupil-labs/pupil/blob/master/pupil_src/shared_modules/file_methods.py#L137-L153
+    [2] https://docs.pupil-labs.com/#data-files
+    """
+    try:
+        offline = os.path.join(directory, 'offline_data', 'offline_' + topic + "_timestamps.npy")
+        if not os.path.isfile(offline):
+            ts_file = os.path.join(directory, topic + "_timestamps.npy")
+            data_ts = np.load(ts_file)
+
+            msgpack_file = os.path.join(directory, topic + ".pldata")
+        else:
+            ts_file = offline
+            data_ts = np.load(ts_file)
+
+            msgpack_file = os.path.join(directory, 'offline_data', 'offline_' + topic + ".pldata")
+
+        with open(msgpack_file, "rb") as fh:
+            unpacker = msgpack.Unpacker(fh, raw=False, use_list=False)
+            for timestamp, (topic, payload) in zip(data_ts, unpacker):
+                datum = deserialize_msgpack(payload)
+
+                # custom extraction function for pupil data, see below for details
+                eye_id, conf, dia_2d, dia_3d = extract_eyeid_diameters(datum)
+                # yield data according to csv_header() sequence
+                yield (eye_id, timestamp, conf, dia_2d, dia_3d)
+    except FileNotFoundError:
+        logger.warning("{} cannot be processed - file does not exist".format(ts_file))
+        return
+    return 
+
+def extract_eyeid_diameters(pupil_datum):
+    """Extract data for a given pupil datum
+    
+    Returns: tuple(eye_id, confidence, diameter_2d, and diameter_3d)
+    """
+    return (
+        pupil_datum["id"],
+        pupil_datum["confidence"],
+        pupil_datum["diameter"],
+        pupil_datum.get("diameter_3d", 0.0)
+    )
+
+def extract_eyeid_messages(datum):
+    """Extract data for a given pupil datum
+    
+    Returns: tuple(eye_id, confidence, diameter_2d, and diameter_3d)
+    """
+    return (
+        datum.get("label", '')
+    )
+
+
+def deserialize_msgpack(msgpack_bytes):
+    """Deserialize msgpack[1] data
+
+    [1] https://msgpack.org/index.html
+    """
+    return msgpack.unpackb(msgpack_bytes, raw=False, use_list=False)
+
+if __name__ == "__main__":
+    # setup logging
+    logging.basicConfig(level=logging.DEBUG)
+
+    # setup command line interface
+    parser = argparse.ArgumentParser(
+        description=(
+            "Extract 2d and 3d (if available) "
+            "pupil diameters for a set of given recordings. "
+            "The resulting csv file will be saved within its "
+            "according recording."
+        )
+    )
+    parser.add_argument(
+        "--recordings", 
+        nargs="+", 
+        help="One or more recordings"
+    )
+
+    parser.add_argument(
+        "--out",
+        nargs = '+',
+        help="CSV file name containing the extracted data",
+        default="pupil_positions.csv",
+    )
+
+    parser.add_argument(
+        "--dir",
+        help="CSV file name containing the extracted data",
+        default='',
+    )
+
+    parser.add_argument(
+        "-f",
+        "--overwrite",
+        action="store_true",
+        help=(
+            "Usually, the command refuses to overwrite existing csv files. "
+            "This flag disables these checks."
+        ),
+    )
+
+    parser.add_argument(
+        "-a",
+        "--annotations",
+        action="store_false",
+        help=(
+            "Usually you want to also extract the annotations"
+            "This flag disables this."
+        ),
+    )
+    
+    # parse command line arguments and start the main procedure
+    args = parser.parse_args()
+    main(recordings=args.recordings, csv_out=args.out, out_directory = args.dir, overwrite=args.overwrite)
